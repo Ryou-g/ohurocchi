7795fea6220d5214337723e13251af52c1b34060
Ryou-g <kd1309305@st.kobedenshi.ac.jp>
2022-10-14 14:49:39 +0900

    title画面とhome画面の大本が完成

--- app/src/main/java/com/example/ohurocchi/HomeActivity.kt
+++ app/src/main/java/com/example/ohurocchi/HomeActivity.kt
@@ -1,18 +1,68 @@
 package com.example.ohurocchi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var mp: MediaPlayer
+
+    // ① 準備（コンポを部屋に置く・コピペOK）
+    private var soundPool // 効果音を鳴らす本体（コンポ）
+            : SoundPool? = null
+    private var mp3a // 効果音データ（mp3）
+            = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val btnBack :Button = findViewById(R.id.btnBack)
 
-        //３）戻るボタン（アクティビティの終了）
-        btnBack.setOnClickListener {
-            finish()
+        // ② 初期化（電源を入れる・コピペOK）
+        soundPool = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            SoundPool(5, AudioManager.STREAM_MUSIC, 0)
+        } else {
+            val attr = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build()
+            SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(5)
+                .build()
         }
+
+        // ③ 読込処理(CDを入れる)
+        mp3a = soundPool!!.load(this, R.raw.voice1, 1)
+        mp = MediaPlayer.create(this,R.raw.bath)
+        mp.isLooping = true
+        mp.start()
+    }
+
+    fun onA(v: View?) {
+        // ④ 再生処理(再生ボタン)
+        soundPool!!.play(mp3a, 1f, 1f, 0, 0, 1f)
+    }
+    //６）再開
+    override fun onResume() {
+        super.onResume()
+        mp.start()
+    }
+    //５）一時停止
+    override fun onPause() {
+        super.onPause()
+        mp.pause()
+    }
+
+    //７）終了・メモリの解放
+    override fun onDestroy() {
+        super.onDestroy()
+        mp.stop() //終了・停止
+        mp.release() //解放
     }
 }
--- app/src/main/java/com/example/ohurocchi/TitleActivity.kt
+++ app/src/main/java/com/example/ohurocchi/TitleActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.media.MediaPlayer
 
 class TitleActivity : AppCompatActivity() {
+    //５）mpを横断的に使えるようにここに書く
+    private lateinit var mp:MediaPlayer
+    private lateinit var mp1:MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
@@ -17,8 +22,35 @@ class TitleActivity : AppCompatActivity() {
         btnStart.setOnClickListener {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
-            //めちゃめちゃなコメント
 
         }
+
+        mp = MediaPlayer.create(this,R.raw.bath)
+        mp1 = MediaPlayer.create(this,R.raw.title)
+        mp.isLooping = true
+        mp.start()
+        mp1.start()
     }
+    //６）再開
+    override fun onResume() {
+        super.onResume()
+        mp.start()
+        mp1.start()
+    }
+    //５）一時停止
+    override fun onPause() {
+        super.onPause()
+        mp.pause()
+        mp1.pause()
+    }
+
+    //７）終了・メモリの解放
+    override fun onDestroy() {
+        super.onDestroy()
+        mp.stop() //終了・停止
+        mp1.stop() //終了・停止
+        mp.release() //解放
+        mp1.release() //解放
+    }
+
 }
