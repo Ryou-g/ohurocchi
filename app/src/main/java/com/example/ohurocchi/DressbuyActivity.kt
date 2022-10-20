6b3081fd9c4835dbbf5d18a6f6cc538cd06ff665
Suzumy <ryouga.0085@gmail.com>
2022-10-20 11:22:38 +0900

    レイアウト調整
    pixel 6で表示できるようにレイアウトを調整

--- app/src/main/java/com/example/ohurocchi/DressbuyActivity.kt
+++ app/src/main/java/com/example/ohurocchi/DressbuyActivity.kt
@@ -1,4 +1,12 @@
 package com.example.ohurocchi
 
-class DressbuyActivity {
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DressbuyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dressbuy)
+    }
 }
