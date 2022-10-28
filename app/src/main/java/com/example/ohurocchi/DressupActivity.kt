4fa6538dca25b97eddce3b1b2c624c1240be28d1
taypayjum <kd1309304@st.kobedenshi.ac.jp>
2022-10-28 09:28:59 +0900

    衣装切り替え機能完成

--- app/src/main/java/com/example/ohurocchi/DressupActivity.kt
+++ app/src/main/java/com/example/ohurocchi/DressupActivity.kt
@@ -1,19 +1,59 @@
 package com.example.ohurocchi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class DressupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dressup)
 
-        val btnBack : Button = findViewById(R.id.btnBack)
+        val btnBack: Button = findViewById(R.id.btnBack)
 
         btnBack.setOnClickListener {
             finish()
 
         }
+
+        val imageView = findViewById<ImageView>(R.id.imageView1)
+        val adult = findViewById<ImageButton>(R.id.adult)
+
+        adult.setOnClickListener {
+
+            imageView.setImageResource(R.drawable.adult)
+
+        }
+
+        val sexy = findViewById<ImageButton>(R.id.sexy)
+
+        sexy.setOnClickListener {
+
+            imageView.setImageResource(R.drawable.sexy)
+
+        }
+
+        val neautral = findViewById<ImageButton>(R.id.neautral)
+
+        neautral.setOnClickListener {
+
+            imageView.setImageResource(R.drawable.neautral)
+
+        }
+
+        val sick = findViewById<ImageButton>(R.id.sick)
+
+        sick.setOnClickListener {
+
+            imageView.setImageResource(R.drawable.sick)
+
+        }
+
     }
+
+
+
 }
