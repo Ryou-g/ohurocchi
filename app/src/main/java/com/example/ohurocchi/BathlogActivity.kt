5ba0c990cab688b1ad0e18d5dbafecc5be6ce5da
benibenisan <72596397+benibenisan@users.noreply.github.com>
2022-11-22 14:50:24 +0900

    入浴ログ画面にカレンダーを追加しました

--- app/src/main/java/com/example/ohurocchi/BathlogActivity.kt
+++ app/src/main/java/com/example/ohurocchi/BathlogActivity.kt
@@ -12,25 +12,6 @@ class BathlogActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bathlog)
 
-        val spinner = findViewById<Spinner>(R.id.spinner)
-        val adapter = ArrayAdapter.createFromResource(this, R.array.month, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-
-        // OnItemSelectedListenerの実装
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-
-            // 項目が選択された時に呼ばれる
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val text = parent?.selectedItem as String
-                Toast.makeText(this@BathlogActivity, text, Toast.LENGTH_SHORT).show()
-            }
-
-            // 基本的には呼ばれないが、何らかの理由で選択されることなく項目が閉じられたら呼ばれる
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-        }
 
         val btnBack : Button = findViewById(R.id.btnBack)
 
