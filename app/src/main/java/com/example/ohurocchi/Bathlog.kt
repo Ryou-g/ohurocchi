6f60af38e61376dc5137425516f631170deac624
Suzumy <ryouga.0085@gmail.com>
2022-11-24 11:15:26 +0900

    add serchDateTime
    日付の絞り込みが完成、ついでにrandom追加

--- app/src/main/java/com/example/ohurocchi/Bathlog.kt
+++ app/src/main/java/com/example/ohurocchi/Bathlog.kt
@@ -1,10 +1,12 @@
 package com.example.ohurocchi
 
+import com.google.firebase.firestore.DocumentId
 import java.util.*
 
 data class Bathlog(
-    @com.google.firebase.firestore.DocumentId
+    @DocumentId
     val id: String = "お風呂に入りました",
     val title: String = "",
-    var createdAt: Date = Date(System.currentTimeMillis()),
+    //var createdAt: Date = Date(System.currentTimeMillis()),
+    val createdAt: String = ""
 )
