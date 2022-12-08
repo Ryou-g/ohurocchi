a3ccea6495c8d43fdcb364756cdef71fcb093462
taypayjum <1309304@st.kobedenshi.ac,jp>
2022-12-08 14:52:04 +0900

    好感度表示

--- app/src/main/java/com/example/ohurocchi/CharaActivity.kt
+++ app/src/main/java/com/example/ohurocchi/CharaActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ProgressBar
@@ -26,6 +27,10 @@ class CharaActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chara)
+
+        val circleprogressBar :ProgressBar = findViewById(R.id.circle_progressBar)
+        circleprogressBar.visibility = View.INVISIBLE
+
 
         val textView8: TextView = findViewById(R.id.textView8)
         val progressBar: ProgressBar = findViewById(R.id.progressber)
